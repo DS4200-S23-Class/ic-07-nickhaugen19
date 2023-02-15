b6865ad8f8f74be4af472a2351bee4c64132e59d
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-15 16:00:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,5 +25,47 @@ d3.csv("data/circles.csv").then((data) => {
       .attr("fill", (d) => {return d.color});
   
 });
+
+// with a scale function
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.r
+
+const FRAME2 = d3.select("#vis2")
+                    .append("svg")
+                      .attr("height", FRAME_HEIGHT)
+                      .attr("width", FRAME_WIDTH)
+                      .attr("class", "frame");
+
+d3.csv("data/circles2.csv").then((data) => {
+  
+  const MAX_X2 = d3.max(data, (d) => {
+                          return parseInt(d.x)
+    
+  });
+  
+  const X_SCALE2 = d3.scaleLinear()
+                      .domain([0, MAX_X2])
+                      .range([0, VIS_WIDTH]);
+  
+  FRAME2.selectAll("points")
+        .data(data)
+        .enter()
+        .append("circle")
+          .attr(cx, (d) => {
+            return (X_SCALE_2(d.x) + MARGINS.left
+                    })
+           .attr("cy", MARGINS.top)
+           .attr("r", 20)
+           .attr("class", "point");
+
+  FRAME2.append("g")
+          .attr("transform", "translate(" +
+                MARGINS.left + "," +
+                (VIS_HEIGHT + MARGINS.top) + ")")
+          .call(d3.axisBottom(X_SCALE2.ticks(4))
+                  .attr("font-size", "20px");
+                });
+  
+  
  
   
