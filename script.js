f7910f7df83493ab9b9c07781227f69e271d903c
nickhaugen19 <71619276+nickhaugen19@users.noreply.github.com>
2023-02-15 16:28:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-// Typically, we use constants for frame dimensions and the frame
 const FRAME_HEIGHT = 200;
 const FRAME_WIDTH = 500; 
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 
-// Let's make a vis with the following data 
 const data2 = [55000, 48000, 27000, 66000, 90000]; 
 
-// We would need an extremely large screen to use data2 values
-// as our cx values. In order for our vis to work on (almost) 
-// any screen, we need to be able to map (i.e. scale) our data
-// values to pixel values. 
-
-// Start with a new frame. This time, we will also set a constant
-// for the width and height of our vis
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
 
@@ -22,22 +13,15 @@ const FRAME3 = d3.select("#vis1")
                     .attr("width", FRAME_WIDTH)
                     .attr("class", "frame"); 
 
-// Now, let's define our scaling function
-
-// find max X
 const MAX_X = d3.max(data2, (d) => { return d; }); 
 console.log("Max x: " +MAX_X);  
 
-// Now, define scale functions that maps our data values 
-// (domain) to pixel values (range)
-const X_SCALE = d3.scaleLinear() // linear scale because we have 
-                              // linear data 
-                  .domain([0, (MAX_X + 10000)]) // add some padding  
+const X_SCALE = d3.scaleLinear() 
+                  .domain([0, (MAX_X + 10000)])
                   .range([0, VIS_WIDTH]); 
 
 console.log("Input: 40000, X_SCALE output: " + X_SCALE(40000));
 
-// Now, we can use X_SCALE to plot our points
 FRAME3.selectAll("points")  
     .data(data2)  
     .enter()       
@@ -47,11 +31,9 @@ FRAME3.selectAll("points")
       .attr("r", 20)
       .attr("class", "point"); 
 
-// We can also use X_SCALE to add an axis to the vis  
-FRAME3.append("g") // g is a "placeholder" svg
+FRAME3.append("g")
       .attr("transform", "translate(" + MARGINS.left + 
-            "," + (VIS_HEIGHT + MARGINS.top) + ")") //moves axis 
-                                                    // within margins 
-      .call(d3.axisBottom(X_SCALE).ticks(4)) // function for generating axis  
-        .attr("font-size", '20px'); // set font size
+            "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+      .call(d3.axisLeft(X_SCALE).ticks(4))
+        .attr("font-size", '20px');
   
